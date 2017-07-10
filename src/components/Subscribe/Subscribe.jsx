3b65de504ed678228a8e30ee5dc1934e415658ed
Kia Farhang <kfarhang0@gmail.com>
2017-07-10 09:42:44 -0500

    Added subscribe CTA component

---
+++ src/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,19 @@
+// @flow
+
+import React, {Component} from 'react';
+
+import './Subscribe.css';
+
+class Subscribe extends Component {
+	render(){
+		return(
+			<div className='SubscribeContainer'>
+				<h4>What if Seinfeld still on tv?</h4>
+				<p>Milenials Are The Reason Seinfeld Is Still Not On TV</p>
+				<a href='https://twitter.com/Seinfeld2000' title='Subscribe to the Express-News'><button>Subscribe now</button></a>
+			</div>
+		)
+	}
+}
+
+export default Subscribe;
