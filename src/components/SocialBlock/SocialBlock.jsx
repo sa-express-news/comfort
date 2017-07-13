1fba7f73dfed077f97aca3ab50875514b5877575
Kia Farhang <kfarhang0@gmail.com>
2017-07-13 11:10:46 -0500

    Swapped out social share links

--- src/components/SocialBlock/SocialBlock.jsx
+++ src/components/SocialBlock/SocialBlock.jsx
@@ -9,8 +9,8 @@ import './SocialBlock.css';
 export default () => (
   <div className="SocialBlock">
     <Social type='logo' link='http://www.expressnews.com/' />
-    <Social type='facebook' link='http://www.expressnews.com/st-anthony-basketball-ticket-to-top/' />
-    <Social type='twitter' link='http://www.expressnews.com/st-anthony-basketball-ticket-to-top/' />
-    <Social type='reddit' link='http://www.expressnews.com/st-anthony-basketball-ticket-to-top/' />
+    <Social type='facebook' link='http://www.expressnews.com/comfort_texas_30_year_anniversary/' />
+    <Social type='twitter' link='http://www.expressnews.com/comfort_texas_30_year_anniversary/' />
+    <Social type='reddit' link='http://www.expressnews.com/comfort_texas_30_year_anniversary/' />
   </div>
 );
